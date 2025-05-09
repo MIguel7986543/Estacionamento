d0c6b018463e55e2ae64e3361461724b57f56f65
Miguel dias <mdias1300@gmail.com>
2025-05-09 20:17:40 -0300

    fazendo ainda

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+double permanencia, valet, veiculo, lavagem, total;
+string P, G, S, N;
+
+Console.WriteLine("--- Estacionamento ---");
+
+Console.WriteLine("Tamanho do veículo (P/G):");
+veiculo = Convert.ToDouble(Console.ReadLine());
+
+ if (veiculo = P);
+ {
+    
+ }
